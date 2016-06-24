a0540787d3639a279ea5a1870cd42704824c12f9
yokoi <yokoi.shinya@jp.fujitsu.com>
2016-06-24 15:09:39 +0900

    update motion

--- motion_sensor.py
+++ motion_sensor.py
@@ -2,14 +2,25 @@ import RPi.GPIO as GPIO
 from time import sleep
 
 sensor = 21
+led = 2
 
+GPIO.cleanup()
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(sensor, GPIO.IN)
+GPIO.setup(led, GPIO.OUT)
 
 try:
-        while True:        
-                print GPIO.input(sensor)
-                sleep(1)
+  while True:        
+    val = GPIO.input(sensor)
+    print GPIO.input(sensor)
+
+    if val == 1:
+      GPIO.output(led, GPIO.HIGH)
+
+    else:
+      GPIO.output(led, GPIO.LOW)
+
+    sleep(1)
 
 except KeyboardInterrupt:
         pass
